f58f177f836061ae03e182bfa6d1069c1705e0b9
brunolandim <brunolandim13@hotmail.com>
2025-04-10 20:03:00 -0300

    remove fields

--- src/models/item.model.ts
+++ src/models/item.model.ts
@@ -4,8 +4,6 @@ import { Schema, Document } from 'mongoose';
 export interface Item extends Document {
   name: string;
   description: string;
-  createdAt?: Date;
-  updatedAt?: Date;
 }
 export const ItemSchema = new Schema<Item>({
   name: { type: String, required: true },
